0852c05cbfd4df80c3f838729d4ee69dec47e768
Juan Pablo Moris <juamfr@gmail.com>
2023-04-28 21:14:47 -0300

    Primera subida

---
+++ WinForms_1/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace WinForms_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblFechaNacimiento = new System.Windows.Forms.Label();
+            this.dtpFechaNacimiento = new System.Windows.Forms.DateTimePicker();
+            this.ckbChocolate = new System.Windows.Forms.CheckBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.radioWizard = new System.Windows.Forms.RadioButton();
+            this.radioMuggle = new System.Windows.Forms.RadioButton();
+            this.radioSquize = new System.Windows.Forms.RadioButton();
+            this.gbTipo = new System.Windows.Forms.GroupBox();
+            this.lwElementos = new System.Windows.Forms.ListView();
+            this.btnVerPerfil = new System.Windows.Forms.Button();
+            this.gbTipo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(520, 310);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(134, 66);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(121, 20);
+            this.txtNombre.TabIndex = 2;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(76, 69);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 3;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblFechaNacimiento
+            // 
+            this.lblFechaNacimiento.AutoSize = true;
+            this.lblFechaNacimiento.Location = new System.Drawing.Point(12, 100);
+            this.lblFechaNacimiento.Name = "lblFechaNacimiento";
+            this.lblFechaNacimiento.Size = new System.Drawing.Size(108, 13);
+            this.lblFechaNacimiento.TabIndex = 4;
+            this.lblFechaNacimiento.Text = "Fecha de Nacimiento";
+            // 
+            // dtpFechaNacimiento
+            // 
+            this.dtpFechaNacimiento.Location = new System.Drawing.Point(134, 100);
+            this.dtpFechaNacimiento.Name = "dtpFechaNacimiento";
+            this.dtpFechaNacimiento.Size = new System.Drawing.Size(200, 20);
+            this.dtpFechaNacimiento.TabIndex = 5;
+            // 
+            // ckbChocolate
+            // 
+            this.ckbChocolate.AutoSize = true;
+            this.ckbChocolate.Location = new System.Drawing.Point(134, 140);
+            this.ckbChocolate.Name = "ckbChocolate";
+            this.ckbChocolate.Size = new System.Drawing.Size(131, 17);
+            this.ckbChocolate.TabIndex = 6;
+            this.ckbChocolate.Text = "te gusta el chocolate?";
+            this.ckbChocolate.UseVisualStyleBackColor = true;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(134, 9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(184, 31);
+            this.lblTitulo.TabIndex = 7;
+            this.lblTitulo.Text = "Perfil Persona";
+            // 
+            // radioWizard
+            // 
+            this.radioWizard.AutoSize = true;
+            this.radioWizard.Location = new System.Drawing.Point(6, 19);
+            this.radioWizard.Name = "radioWizard";
+            this.radioWizard.Size = new System.Drawing.Size(58, 17);
+            this.radioWizard.TabIndex = 8;
+            this.radioWizard.TabStop = true;
+            this.radioWizard.Text = "Wizard";
+            this.radioWizard.UseVisualStyleBackColor = true;
+            // 
+            // radioMuggle
+            // 
+            this.radioMuggle.AutoSize = true;
+            this.radioMuggle.Location = new System.Drawing.Point(97, 19);
+            this.radioMuggle.Name = "radioMuggle";
+            this.radioMuggle.Size = new System.Drawing.Size(60, 17);
+            this.radioMuggle.TabIndex = 9;
+            this.radioMuggle.TabStop = true;
+            this.radioMuggle.Text = "Muggle";
+            this.radioMuggle.UseVisualStyleBackColor = true;
+            // 
+            // radioSquize
+            // 
+            this.radioSquize.AutoSize = true;
+            this.radioSquize.Location = new System.Drawing.Point(188, 19);
+            this.radioSquize.Name = "radioSquize";
+            this.radioSquize.Size = new System.Drawing.Size(57, 17);
+            this.radioSquize.TabIndex = 10;
+            this.radioSquize.TabStop = true;
+            this.radioSquize.Text = "Squize";
+            this.radioSquize.UseVisualStyleBackColor = true;
+            // 
+            // gbTipo
+            // 
+            this.gbTipo.Controls.Add(this.radioWizard);
+            this.gbTipo.Controls.Add(this.radioSquize);
+            this.gbTipo.Controls.Add(this.radioMuggle);
+            this.gbTipo.Location = new System.Drawing.Point(134, 163);
+            this.gbTipo.Name = "gbTipo";
+            this.gbTipo.Size = new System.Drawing.Size(279, 51);
+            this.gbTipo.TabIndex = 11;
+            this.gbTipo.TabStop = false;
+            this.gbTipo.Text = "Tipo";
+            // 
+            // lwElementos
+            // 
+            this.lwElementos.HideSelection = false;
+            this.lwElementos.Location = new System.Drawing.Point(140, 278);
+            this.lwElementos.Name = "lwElementos";
+            this.lwElementos.Size = new System.Drawing.Size(139, 55);
+            this.lwElementos.TabIndex = 0;
+            this.lwElementos.UseCompatibleStateImageBehavior = false;
+            this.lwElementos.View = System.Windows.Forms.View.List;
+            // 
+            // btnVerPerfil
+            // 
+            this.btnVerPerfil.Location = new System.Drawing.Point(134, 231);
+            this.btnVerPerfil.Name = "btnVerPerfil";
+            this.btnVerPerfil.Size = new System.Drawing.Size(75, 23);
+            this.btnVerPerfil.TabIndex = 12;
+            this.btnVerPerfil.Text = "Ver Perfil";
+            this.btnVerPerfil.UseVisualStyleBackColor = true;
+            this.btnVerPerfil.Click += new System.EventHandler(this.btnVerPerfil_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 345);
+            this.Controls.Add(this.btnVerPerfil);
+            this.Controls.Add(this.gbTipo);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.ckbChocolate);
+            this.Controls.Add(this.dtpFechaNacimiento);
+            this.Controls.Add(this.lblFechaNacimiento);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.lwElementos);
+            this.Name = "Form1";
+            this.Text = "Titulo App";
+            this.gbTipo.ResumeLayout(false);
+            this.gbTipo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblFechaNacimiento;
+        private System.Windows.Forms.DateTimePicker dtpFechaNacimiento;
+        private System.Windows.Forms.CheckBox ckbChocolate;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.RadioButton radioWizard;
+        private System.Windows.Forms.RadioButton radioMuggle;
+        private System.Windows.Forms.RadioButton radioSquize;
+        private System.Windows.Forms.GroupBox gbTipo;
+        private System.Windows.Forms.ListView lwElementos;
+        private System.Windows.Forms.Button btnVerPerfil;
+    }
+}
+
---
+++ WinForms_1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string elem = txtNombre.Text;
+            lwElementos.Items.Add(elem);
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVerPerfil_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            DateTime fecha = dtpFechaNacimiento.Value;
+            string chocolate = ckbChocolate.Checked == true ? "Le gusta el chocolate" : "No le gusta el chocolate";
+            string tipo;
+            if (radioMuggle.Checked == true)
+            {
+                tipo = "Muggle";
+            }
+            else if (radioWizard.Checked == true)
+            {
+                tipo = "Wizard";
+            }
+            else tipo = radioSquize.Text;
+            // operador ternario
+            MessageBox.Show("Nombre: " + nombre + " | Fecha de Nacimiento: " + fecha + "\n"+ chocolate + " y es " + tipo);
+            
+        }
+    }
+}
