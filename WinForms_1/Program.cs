acc4b262aa1b10388225bb95763203b530253e24
Juan Pablo Moris <juamfr@gmail.com>
2023-04-28 21:24:47 -0300

    Modificacion del program.cs

--- WinForms_1/Program.cs
+++ WinForms_1/Program.cs
@@ -17,6 +17,8 @@ namespace WinForms_1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            // modificacion del archivo
+            // otro comentario
         }
     }
 }
